21d59667400676b5d0be2b1b29c64ddd2817e47f
Your Name <your.email@example.com>
2025-11-10 08:50:52 -0500

    HU-3: Creación de módulos de usuarios, productos, y clientes usando el CLI de Nest. Y Uso de DTOs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor (private readonly userService: UsersService){}
+
+    @Post()
+    create(@Body() dto: CreateUserDto){
+        return this.userService.createUser(dto);
+    }
+
+    @Get()
+    findAllUsers(){
+        return this.userService.findAllUsers();
+    }
+
+    @Get(':id')
+    findUserById(@Param('id') id: string) {
+    return this.userService.findUserById(id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepo: Repository<User>
+    ){}
+
+    createUser(dto: CreateUserDto){
+        const user = this.userRepo.create(dto);
+        return this.userRepo.save(user);
+    }
+
+    findAllUsers(){
+        return this.userRepo.find();
+    }
+
+    findUserById(id) {
+    return this.userRepo.findOneBy({ id });
+  }
+}
