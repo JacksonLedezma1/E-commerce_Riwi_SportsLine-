924d9a955572f6af3cee31cb5c8ad0475a219819
juandhm18 <juandhm20@gmail.com>
2025-11-18 08:12:56 -0500

    HU-4: Middleware, filtros e interceptores

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -2,11 +2,13 @@ import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Controller('orders')
 export class OrderController {
     constructor(private readonly orderService: OrderService) {}
 
+    @Roles('admin', 'seller')
     @Post()
     createOrder(@Body() dto: CreateOrderDto) {
         return this.orderService.createOrder(dto);
@@ -22,6 +24,7 @@ export class OrderController {
         return this.orderService.findOrderById(id);
     }
 
+    @Roles('admin', 'seller')
     @Put(':id')
     updateOrder(@Param('id') id: number, @Body() dto: UpdateOrderDto) {
         return this.orderService.updateOrder(id, dto);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,11 +2,13 @@ import { Body, Controller, Get, Post, Param, Put, Delete } from '@nestjs/common'
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Controller('users')
 export class UsersController {
     constructor (private readonly userService: UsersService){}
 
+    @Roles('admin')
     @Post()
     create(@Body() dto: CreateUserDto){
         return this.userService.createUser(dto);
@@ -22,11 +24,13 @@ export class UsersController {
     return this.userService.findUserById(id);
     }
 
+    @Roles('admin')
     @Put(':id')
     updateUser(@Param('id') id: number, @Body() dto: UpdateUserDto) {
         return this.userService.updateUser(id, dto);
     }
 
+    @Roles('admin')
     @Delete(':id')
     deleteUser(@Param('id') id: number) {
         return this.userService.deleteUser(id);
